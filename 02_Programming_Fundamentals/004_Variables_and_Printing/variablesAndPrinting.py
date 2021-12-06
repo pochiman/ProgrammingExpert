8d0047d56b5fa04982c49d1d0f6479889c9eb8dd
pochiman <pochiman@yahoo.com>
2021-12-05 23:15:58 -0500

    initial commit

---
+++ 02_Programming_Fundamentals/004_Variables_and_Printing/variablesAndPrinting.py
@@ -0,0 +1,35 @@
+print("hello\n", 5, True)
+print("tim")
+
+print("\n", 5, True)
+print("tim")
+
+print(5, True, end="\n")  # This is the end default value.
+print("tim")
+
+print(5, True, end=" | ")
+print("tim")
+
+print(5, True, end="hello")
+print("tim")
+
+x = 3
+print(x)
+y = "hello"
+print(y)
+num = y
+print(num)
+y = x
+print(y)
+x = 4
+print(x)
+
+print(x, y)
+print(num)
+print(num, y)
+
+name = "Tim"
+Name = "Bill"
+
+print(name)
+print(nam)
