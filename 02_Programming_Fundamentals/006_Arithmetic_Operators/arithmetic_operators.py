694cd08486044b8763762b36a2721d940e218433
pochiman <pochiman@yahoo.com>
2021-12-11 21:30:23 -0500

    initial commit

---
+++ 02_Programming_Fundamentals/006_Arithmetic_Operators/arithmetic_operators.py
@@ -0,0 +1,103 @@
+x = 1.0
+y = 2
+
+result = x + y
+print(result)
+
+x = -1
+y = -2.0
+
+result = x - y
+print(result)
+
+x = -1
+y = -2.0
+
+result = 4 - (-8)
+print(result)
+
+x = -1
+y = -2.0
+
+result = 4 * (-8)
+print(result)
+
+x = -1
+y = -2.0
+
+result = x * y
+print(result)
+
+x = 10
+y = 2
+
+result = x / y
+print(result)
+
+x = 10
+y = 2.5
+
+result = x / y
+print(result)
+
+# x = 10
+# y = 0
+
+# result = x / y
+# print(result)
+
+x = 10
+y = 2
+
+result = x ** y
+print(result)
+
+x = 10
+y = 0
+
+result = x ** y
+print(result)
+
+x = 10
+y = -1
+
+result = x ** y
+print(result)
+
+x = 11
+y = 3
+
+result = x // y
+print(result)
+
+x = 11
+y = 2
+
+result = x % y
+print(result)
+
+x = 11
+y = 2
+z = 4
+
+# result = x % y / 4 * 7 ** 2 / 3
+result = (x + y - z) ** (x % z)
+print(result)
+
+result = (y % z) * 2 / 4
+print(result)
+
+result = (5 + x - z) ** y - 7 / 2 // 4
+print(result)
+
+print(3.5 // 4)
+# brackets / parenthesis
+# exponents
+# multiplication and division and modulo
+# addition subtraction
+
+answer = 4.5 / True
+print(answer)
+
+# answer = 4.5 / False
+# print(answer)
