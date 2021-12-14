05b1338899ebeebbbd6cbab327ff3d7ee33b24ed
pochiman <pochiman@yahoo.com>
2021-12-13 23:15:01 -0500

    initial commit

---
+++ 02_Programming_Fundamentals/008_Conditions/conditions.py
@@ -0,0 +1,136 @@
+cond = 2 == 3
+print(cond)
+
+cond = 3 == 3
+print(cond)
+
+cond = 3.0 == 3
+print(cond)
+
+cond = 3.0 != 3
+print(cond)
+
+cond = 3.1 != 3
+print(cond)
+
+x = 4
+y = 4
+
+cond = x != y
+
+print(cond)
+
+cond = x != 9
+
+print(cond)
+
+cond = x < 9
+
+print(cond)
+
+x = 9
+y = 4
+
+cond = x < 9
+
+print(cond)
+
+cond = x < 9.5
+
+print(cond)
+
+cond = x > 9.5
+
+print(cond)
+
+cond = x + 2 > 9.5
+
+print(cond)
+
+cond = x + 2 > 9.5 + 1
+
+print(cond)
+
+cond = x <= 9
+
+print(cond)
+
+cond = x >= 9
+
+print(cond)
+
+cond = x == "6"
+
+print(cond)
+
+cond = x != "6"
+
+print(cond)
+
+str1 = "hello"
+str2 = "Hello"
+
+cond = str1 == str2
+
+print(cond)
+
+cond = str1 != str2
+
+print(cond)
+
+str1 = "hello"
+str2 = "hello "
+
+cond = str1 == str2
+
+print(cond)
+
+# str1 = "ac"
+# str2 = "ab"
+
+# cond = str1 <= str2
+
+# print(cond)
+
+print(ord("a"))
+print(ord("A"))
+print(ord("!"))
+# "a" - 97
+# "A" - 65
+
+print(chr(76))
+print(chr(98))
+
+str1 = "ABc"
+str2 = "ABC"
+
+cond = str1 <= str2
+
+print(cond)
+
+str1 = "ABc"
+str2 = "ABC"
+
+cond = str1 > str2
+
+print(cond)
+
+cond = True == 1
+
+print(cond)
+
+cond = False == 0
+
+print(cond)
+
+cond = False == ""
+
+print(cond)
+
+cond = True == ""
+
+print(cond)
+
+cond = True == "True"
+
+print(cond)
