c15beb54669effc092bf45b9616f88569fef851c
pochiman <pochiman@yahoo.com>
2021-12-17 12:23:51 -0500

    initial commit

---
+++ 02_Programming_Fundamentals/008_Conditions/solution.py
@@ -0,0 +1,28 @@
+# Set the values of w, x, y, and z such that all of the condition_ variables evaluate to True.
+# Only use integer values.
+
+'''
+w = -1  # <- Change this
+x = -1  # <- Change this
+y = -1  # <- Change this
+z = -1  # <- Change this
+'''
+
+w = 1
+x = 1
+y = 3
+z = 2
+
+# Don't change any of these `condition_` variables.
+condition_1 = w < x + y
+condition_2 = x == y - 2
+condition_3 = z != 0
+condition_4 = z + z == z * z
+condition_5 = w > 0
+
+# All of these should print `True`.
+print(condition_1)
+print(condition_2)
+print(condition_3)
+print(condition_4)
+print(condition_5)
