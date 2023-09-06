0afe7a4f0c181f87a197fe1f492dd3dda094e1d1
pochiman <pochiman@yahoo.com>
2023-09-06 16:51:51 -0400

    initial commit

---
+++ 02_Programming_Fundamentals/06_Arithmetic_Operators/arithmetic_operators.md
@@ -0,0 +1,24 @@
+# Key Term
+
+# [Operator]
+An `operator` is a symbol that indicates an operation to be performed.
+`+, -, /, *, **, //` are all operators.
+
+# [Operand]
+An `operand` is an object that an operator acts on. In the expression `2 + 3` 
+both `2` and `3` are operands while `+ ` is the operator.
+
+# [Integer_Division]
+`Integer division` is division in which the fractional part (remainder) is discarded. 
+In Python, integer division is performed by using the `//` operator. For example, the 
+expression `11 // 3` evalualtes to `3`.
+
+# [Modulus]
+The `modulus` (or modulo) is the remainder after dividing one number by another. 
+In Python, modulus is performed by using the % operator. For example, the expression 
+`10 % 3` evaluates to `1`.
+
+# [Concatenation]
+In Python, `concatenation` is the process of combining or adding strings together. 
+Concatenation can be performed using the `+` operator. For example, the expression 
+`"1" + "word"` evaluates to `"1word"`.
--- 02_Programming_Fundamentals/06_Arithmetic_Operators/arithmetic_operators.py
+++ 02_Programming_Fundamentals/06_Arithmetic_Operators/arithmetic_operators.py
@@ -37,7 +37,7 @@ print(result)
 x = 10
 y = 2.5
 
-result = x / y
+result = x / y  # regular division; results in a float along with its remainder, if any
 print(result)
 
 # x = 10
@@ -49,7 +49,7 @@ print(result)
 x = 10
 y = 2
 
-result = x ** y
+result = x ** y  # exponential
 print(result)
 
 x = 10
@@ -67,13 +67,13 @@ print(result)
 x = 11
 y = 3
 
-result = x // y
+result = x // y  # integer division; converting whatever the float is to an integer without its remainder, if any
 print(result)
 
 x = 11
 y = 2
 
-result = x % y
+result = x % y  # modulus division; gives you the remainder after division
 print(result)
 
 x = 11
@@ -82,22 +82,36 @@ z = 4
 
 # result = x % y / 4 * 7 ** 2 / 3
 result = (x + y - z) ** (x % z)
-print(result)
+# result = 9 ** 3
+print(result)  # 729
 
 result = (y % z) * 2 / 4
-print(result)
+# result = 2 * 2 / 4
+print(result)  # 1.0
 
 result = (5 + x - z) ** y - 7 / 2 // 4
-print(result)
+# result = 12 ** y - 7 / 2 // 4
+# result = 144 - 7 / 2 // 4
+# result = 144 - 3.5 // 4
+# result = 144 - 0.0
+print(result)  # 144.0
 
 print(3.5 // 4)
-# brackets / parenthesis
-# exponents
-# multiplication and division and modulo
-# addition subtraction
+#####  operator precedence #####
+# [ 1st ] brackets / parenthesis
+# [ 2nd ] exponents
+# [ 3rd ] multiplication and division and modulus
+# [ 4th ] addition subtraction
+
+# if operators of the same precedence are present, we're going to execute them in the 
+# order in which they occur / whatever comes first in the expression; left to right
+# whatever comes to the left first and then whatever is on the right last
+
+answer = "5" + "4"
+print(answer)  # 54
 
 answer = 4.5 / True
-print(answer)
+print(answer)  # 4.5
 
 # answer = 4.5 / False
-# print(answer)
+# print(answer)  # ZeroDivisionError
