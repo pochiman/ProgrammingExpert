b6855787ed88ce9c478cec7904049178b73fed61
pochiman <pochiman@yahoo.com>
2023-11-08 14:50:45 -0500

    initial commit

--- 02_Programming_Fundamentals/15_While_Loops/while_loops.py
+++ 02_Programming_Fundamentals/15_While_Loops/while_loops.py
@@ -1,40 +1,40 @@
-# x = 0
-# while x < 5:
-#   print(x)
-#   x += 1
+x = 0
+while x < 5:
+    print(x)  # 0 1 2 3 4
+    x += 1
 
 ##### OR #####
 
-# for x in range(5):
-#   print(x)
+for x in range(5):
+    print(x)
 
 ########## ########## ########## ########## ##########
 
-# num = input("Enter an integer: ")
+num = input("Enter an integer: ")
 
-# while not num.isdigit():
-#   num = input("Enter an integer: ")
-
-########## ########## ########## ########## ##########
-
-# while True:
-#   num = input("Enter an integer: ")
-#   if num.isdigit():
-#     break
+while not num.isdigit():
+    num = input("Enter an integer: ")
 
 ########## ########## ########## ########## ##########
 
-# lst = [2, 3, 3, -2, -2, -1]
+while True:
+    num = input("Enter an integer: ")
+    if num.isdigit():
+        break
 
-# result = 0
-# i = 0
+########## ########## ########## ########## ##########
 
-# while result < 9 and i < len(lst):
-#   num = lst[i]
-#   result += num
-#   i += 1
+lst = [2, 3, 3, -2, -2, -1]
 
-#   print(num)
+result = 0
+i = 0
+
+while result < 9 and i < len(lst):
+    num = lst[i]
+    result += num
+    i += 1
+
+    print(num)
 
 ########## ########## ########## ########## ##########
 
@@ -42,9 +42,9 @@ lst = [2, 3, 3, -2, -2, -1]
 i = 0
 
 while i < len(lst):
-  if lst[i] == -2:
-    print("found it")
-    break
-  i += 1
+    if lst[i] == -2:
+        print("found it")
+        break
+    i += 1
 else:
-  print("didn't find it")
+    print("didn't find it")
