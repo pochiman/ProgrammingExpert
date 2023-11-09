149c8061494f80c044ee6b7f16ac037ff476a400
pochiman <pochiman@yahoo.com>
2023-11-09 18:17:54 -0500

    initial commit

--- 02_Programming_Fundamentals/17_Dictionaries/dictionaries.py
+++ 02_Programming_Fundamentals/17_Dictionaries/dictionaries.py
@@ -1,167 +1,174 @@
-# x = {2: "hello", "1": 5}
+x = {2: "hello", "1": 5}
 
-# print(x[2])
-# print(x["1"])
-
-########## ########## ########## ########## ##########
-
-# x = {}
-
-# x["key"] = "value"
-
-# print(x)
-# print(x["key"])
+print(x[2])     # hello
+print(x["1"])   # 5
 
 ########## ########## ########## ########## ##########
 
-# x = {"key": 1}
+x = {}
 
-# x["key"] = "value"
+x["key"] = "value"
 
-# print(x["key"])
-
-########## ########## ########## ########## ##########
-
-# x = {"key": 1}
-
-# x["key2"] = "value"
-
-# print(x)
+print(x)        # {'key': 'value'}
+print(x["key"]) # value
 
 ########## ########## ########## ########## ##########
 
-# x = dict()
+x = {"key": 1}
 
-# x["key2"] = "value"
+x["key"] = "value"
 
-# print(x["key2"])
-
-########## ########## ########## ########## ##########
-
-# x = {1: 1, 2: 2, 3: 3}
-
-# del x[1]
-
-# print(x)
+print(x["key"]) # value
 
 ########## ########## ########## ########## ##########
 
-# x = {1: 1, 2: 2, 3: 3}
+x = {"key": 1}
 
-# contains = 1 in x
+x["key2"] = "value"
 
-# print(contains)
-
-########## ########## ########## ########## ##########
-
-# x = {2: 1, 3: 3}
-
-# values = x.values()
-
-# print(values)
+print(x)        # {'key': 1, 'key2': 'value'}
 
 ########## ########## ########## ########## ##########
 
-# x = {2: 1, 3: 3}
+x = dict()
 
-# values = list(x.values())
+x["key2"] = "value"
 
-# print(values[0])
-
-########## ########## ########## ########## ##########
-
-# x = {2: 1, 3: 3}
-
-# keys = x.keys()
-
-# print(keys)
+print(x["key2"])    # value
 
 ########## ########## ########## ########## ##########
 
-# x = {2: 1, 3: 3}
+x = {1: 1, 2: 2, 3: 3}
 
-# keys = list(x.keys())
+del x[1]
 
-# print(keys[0])
-
-########## ########## ########## ########## ##########
-
-# x = {2: 1, 3: 3}
-
-# items = x.items()
-
-# print(items)
+print(x)        # {2: 2, 3: 3}
 
 ########## ########## ########## ########## ##########
 
-# x = {2: 1, 3: 3}
+x = {1: 1, 2: 2, 3: 3}
 
-# items = list(x.items())
+contains = 1 in x
 
-# print(items)
-
-########## ########## ########## ########## ##########
-
-# x = {2: 1, 3: 3}
-
-# print(len(x))
+print(contains) # True
 
 ########## ########## ########## ########## ##########
 
-# x = {2: 1, 3: 3}
+x = {2: 1, 3: 3}
 
-# for key in x:
-#   value = x[key]
-#   print(key, value)
+values = x.values()
 
-########## ########## ########## ########## ##########
-
-# x = {2: 1, 3: 3}
-
-# for key, value in x.items():
-#   print(key, value)
+print(values)   # dict_values([1, 3])
 
 ########## ########## ########## ########## ##########
 
-# x = {2: 1, 3: 3}
+x = {2: 1, 3: 3}
 
-# x[4] = x.get(4, 0) + 1
+values = list(x.values())
 
-# print(x)
-
-########## ########## ########## ########## ##########
-
-# x = {2: 1, 3: 3, 4: 1}
-
-# x[4] = x.get(4, 0) + 1
-
-# print(x)
+print(values[0])    # 1
 
 ########## ########## ########## ########## ##########
 
-# characters = {}
+x = {2: 1, 3: 3}
 
-# string = "hello world"
+keys = x.keys()
 
-# for char in string:
-#   characters[char] = characters.get(char, 0) + 1
-
-# print(characters)
+print(keys) # dict_keys([2, 3])
 
 ########## ########## ########## ########## ##########
 
-# counts = {}
+x = {2: 1, 3: 3}
 
-# while True:
-#   num = input("Number (enter q to quit): ")
+keys = list(x.keys())
 
-#   if num == "q":
-#     break
+print(keys[0])  # 2
 
-#   num = int(num)
-#   counts[num] = counts.get(num, 0) + 1
+########## ########## ########## ########## ##########
 
-# print(counts)
+x = {2: 1, 3: 3}
+
+items = x.items()
+
+print(items)    # dict_items([(2, 1), (3, 3)])
+
+########## ########## ########## ########## ##########
+
+x = {2: 1, 3: 3}
+
+items = list(x.items())
+
+print(items)    # [(2, 1), (3, 3)]
+
+########## ########## ########## ########## ##########
+
+x = {2: 1, 3: 3}
+
+print(len(x))   # 2
+
+########## ########## ########## ########## ##########
+
+x = {2: 1, 3: 3}
+
+for key in x:
+    value = x[key]
+    print(key, value)
+
+# 2
+# 2 1
+# 3 3
+
+########## ########## ########## ########## ##########
+
+x = {2: 1, 3: 3}
+
+for key, value in x.items():
+    print(key, value)
+
+# 2 1
+# 3 3
+
+########## ########## ########## ########## ##########
+
+x = {2: 1, 3: 3}
+
+x[4] = x.get(4, 0) + 1
+
+print(x)    # {2: 1, 3: 3, 4: 1}
+
+########## ########## ########## ########## ##########
+
+x = {2: 1, 3: 3, 4: 1}
+
+x[4] = x.get(4, 0) + 1
+
+print(x)    # {2: 1, 3: 3, 4: 2}
+
+########## ########## ########## ########## ##########
+
+characters = {}
+
+string = "hello world"
+
+for char in string:
+    characters[char] = characters.get(char, 0) + 1
+
+print(characters)   # {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
+
+########## ########## ########## ########## ##########
+
+counts = {}
+
+while True:
+    num = input("Number (enter q to quit): ")
+
+    if num == "q":
+        break
+
+    num = int(num)
+    counts[num] = counts.get(num, 0) + 1
+
+print(counts)
 
 ########## ########## ########## ########## ##########
 
